11089344dc50ae27d5f68769471f33ff2748deaa
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2023-11-13 11:25:36 +0900

    九九計算ファイル・九九実行

--- for.c
+++ for.c
@@ -10,4 +10,16 @@ void main(){
   }
   ave = (float)sum /10.0;
   printf("⇒合計値：%d ,平均値：%2.2f \n",sum,ave);
+
+  int x,y;
+  printf("・九九の計算\n");
+  for(x=1;x<10;x++){
+    printf("対象:%d ⇒",x);
+
+    for(y=1;y<10;y++){
+      printf("%d ",x*y);
+    }
+
+    printf("\n");
+  }
 }
