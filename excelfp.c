632dbef43078d0dff1b652404a1ffbef4b6efe3a
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2023-11-20 15:12:22 +0900

    excelデータ用ファイル・データ実行

---
+++ excelfp.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+
+int main(){
+  float t,y;
+  FILE *fp;
+  fp = fopen("wave.dat","w");
+  if(fp==NULL){
+    printf("We can't open result.dat");
+    exit(1);
+  }
+  for(t=0.0;t<1.0;t+=0.01){
+    y = sin(2*M_PI*4*t);
+    fprintf(fp,"%f\t%f\n",t,y);
+  }
+  fclose(fp);
+  return 0;
+}
