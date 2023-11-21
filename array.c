fdeb16a1822c4a452be649dd09fe8e37f356a9fe
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2023-11-21 17:22:18 +0900

    配列コピー使い方

--- array.c
+++ array.c
@@ -5,12 +5,13 @@ int main(){
   int i;
   int a[10]={1,2,3,4,5,6,7,8,9,10};
   int b[i];
+
+  memcpy(b,a,sizeof(a)); // コピー関数(<引数> コピー先,コピー元,サイズ関数(<引数>配列名))
   // for(i=0;i<10;i++){
   //   b[i] = a[i]*a[i];
   // }
-  memcpy(b,a,sizeof(a));
+
   for(i=0;i<10;i++){
     printf("%d番目 a:%d b:%d \n",i+1,a[i],b[i]);
   }
-
 }
